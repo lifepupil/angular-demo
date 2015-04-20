993fe3a607e4147ac3c9c91ee6ca7ff3f0c34a55
Christian Richard <lifepupil@gmail.com>
2015-04-20 11:50:27 -0700

    added run, beta and gamma

--- client/index.js
+++ client/index.js
@@ -1,8 +1,27 @@
 'use strict';
 
+angular.module('alpha',[])
+.run(['$rootScope', function($rootScope) {
+  console.info('running the run function');
+  $rootScope.z  =12;
+}])
+.controller('beta',['$scope', function($scope){
+  // console.log('beat controller initialized');
+  // console.warn('beat controller initialized');
+  // console.error('beat controller initialized');
+  console.info('beta controller initialized');
+  $scope.x = 3;
+  $scope.y = 2;
+}])
+.controller('gamma',['$scope', function($scope){
+  console.info('gamma controller initialized');
+  $scope.x = 5;
+  $scope.y = 7;
+}]);
+
 
 $(document).ready(init);
 
 function init(){
-  
+
 }
